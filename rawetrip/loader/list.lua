9eb9ba0e60893cc731bc498c0f53c9572cf4f5b4
kl1ent <77514334+kl1ent@users.noreply.github.com>
2022-06-03 18:46:12 +0700

    Update list.lua

--- rawetrip/loader/list.lua
+++ rawetrip/loader/list.lua
@@ -1,1 +1,1 @@
-{name = "Solus UI", link = "https://raw.githubusercontent.com/kl1ent/CS-GO-Scripting/main/rawetrip/Solus%20UI/Solus%20UI.lua", aviable = false}
+{name = "Solus UI", link = "https://raw.githubusercontent.com/kl1ent/CS-GO-Scripting/main/rawetrip/Solus%20UI/Solus%20UI.lua", aviable = false, author = "Klient#1690"}
