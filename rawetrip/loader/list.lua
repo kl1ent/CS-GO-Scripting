796a80af3d753b35f1a85b0f2a027e21f5491e30
kl1ent <77514334+kl1ent@users.noreply.github.com>
2022-06-05 01:00:53 +0700

    Update list.lua

--- rawetrip/loader/list.lua
+++ rawetrip/loader/list.lua
@@ -1,30 +1,48 @@
 {
-  author = "Klient#1690", 
+  update = "25.03.2022",
+  author = "Klient#1690",
+  
   name = "Solus UI", 
+  
   link = "https://raw.githubusercontent.com/kl1ent/CS-GO-Scripting/main/rawetrip/Solus%20UI/Solus%20UI.lua", 
+  
   aviable = true
 },
 
 {
+  update = "30.04.2022",
   author = "Klient#1690", 
+  
   name = "Necron", 
+  
   link = "https://raw.githubusercontent.com/kl1ent/CS-GO-Scripting/main/rawetrip/Necron/Necron.lua", 
+  
   aviable = true
 },
 
 {
+  update = "03.06.2022",
   author = "Klient#1690", 
+  
   name = "Helper", 
+  
   link = "https://raw.githubusercontent.com/kl1ent/CS-GO-Scripting/main/rawetrip/Helper/Helper.lua", 
+  
   aviable = true
 },
 
 {
   class = 2,
+  
+  update = "03.06.2022",
   author = "unknown", 
+  
   second_name = "Helper Data", 
   name = "helper_data", 
+  
   type = "json",
+  
   link = "https://raw.githubusercontent.com/kl1ent/CS-GO-Scripting/main/rawetrip/Helper/helper_data.json", 
+  
   aviable = true
 },
