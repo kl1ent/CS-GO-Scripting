ac764b42a60fb5063acfb5a72c2d78f062b336eb
kl1ent <77514334+kl1ent@users.noreply.github.com>
2022-06-07 22:45:21 +0700

    Update list.lua

--- rawetrip/loader/list.lua
+++ rawetrip/loader/list.lua
@@ -34,6 +34,7 @@
     images = {
       {name = "helper_aim", url = "https://raw.githubusercontent.com/kl1ent/CS-GO-Scripting/main/rawetrip/loader/assets/helper/helper_aim.png", size = vector2d.new(376, 173)},
       {name = "helper_molotov", url = "https://raw.githubusercontent.com/kl1ent/CS-GO-Scripting/main/rawetrip/loader/assets/helper/helper_molotov.png", size = vector2d.new(618, 276)}
+      {name = "helper_menu_1", url = "https://raw.githubusercontent.com/kl1ent/CS-GO-Scripting/main/rawetrip/loader/assets/helper/helper_menu_1.png", size = vector2d.new(713, 507)}
     },
     updates = {
       {"1. Reworked movement for grenades\n2. Added several types of throw (Rage, Legit)\n3. Reworked design\n4. Added support for movement locations"}
