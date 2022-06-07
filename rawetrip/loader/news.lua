4ffe96281f2945bf6100af8dead43719b51600a9
kl1ent <77514334+kl1ent@users.noreply.github.com>
2022-06-07 18:35:33 +0700

    Update news.lua

--- rawetrip/loader/news.lua
+++ rawetrip/loader/news.lua
@@ -1,6 +1,8 @@
 {
-  name = "Update 1"
-  version = "1.1.0"
-  image_url = "",
-  text = "1. Added a block with news\n2. Added scrolling when overflowing"
+  {
+    name = "Update 1"
+    version = "1.1.0"
+    image_url = "",
+    text = "1. Added a block with news\n2. Added scrolling when overflowing"
+  }
 }
