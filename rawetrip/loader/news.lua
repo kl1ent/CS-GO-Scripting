ecf9762e52b7b5fb905925a8338dff09da1aebc2
kl1ent <77514334+kl1ent@users.noreply.github.com>
2022-06-07 18:43:23 +0700

    Update news.lua

--- rawetrip/loader/news.lua
+++ rawetrip/loader/news.lua
@@ -1,7 +1,7 @@
 {
   {
-    name = "Update 1"
-    version = "1.1.0"
+    name = "Update 1",
+    version = "1.1.0",
     image_url = "",
     text = "1. Added a block with news\n2. Added scrolling when overflowing"
   }
